13cec5979c9f72ab34a8e0aab4fbc44446046322
RonnelSalgado1 <80131320+RonnelSalgado1@users.noreply.github.com>
2024-11-01 17:17:28 +0000

    installed chakraUI and added a few components

--- mywellbeing/src/app/page.js
+++ mywellbeing/src/app/page.js
@@ -1,12 +1,33 @@
+"use client"
+
+import { Button } from "@chakra-ui/react"
 
 
 
-
+import { HStack } from "@chakra-ui/react"
+import {
+  ProgressBar,
+  ProgressLabel,
+  ProgressRoot,
+  ProgressValueText,
+} from "@/components/ui/progress"
 
 
 
 export default function Home() {
   return (
+    <div>
     <h1>Hello world</h1>
+    <Button>Button</Button>
+
+    <ProgressRoot defaultValue={40} maxW="sm">
+      <HStack gap="5">
+        <ProgressLabel>Usage</ProgressLabel>
+        <ProgressBar flex="1" />
+        <ProgressValueText>40%</ProgressValueText>
+      </HStack>
+    </ProgressRoot>
+
+    </div>
   )
 }
