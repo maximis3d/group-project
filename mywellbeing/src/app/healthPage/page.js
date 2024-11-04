99fd0d088cf6ea26ef814b714685ddef6b032c5c
RonnelSalgado1 <80131320+RonnelSalgado1@users.noreply.github.com>
2024-11-04 13:56:00 +0000

    installed Recharts and added scatter graph to health page

--- mywellbeing/src/app/healthPage/page.js
+++ mywellbeing/src/app/healthPage/page.js
@@ -1,9 +1,23 @@
+"use client"
+
 import { Link, Text, Button, HStack, Stack } from "@chakra-ui/react";
 import Image from 'next/image';
 import { Field } from "@/components/ui/field";
 import { Input } from "@chakra-ui/react";
+import React, { PureComponent } from 'react';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 export default function healthPage() {
+  const data = [
+    { x: 100, y: 200, z: 200 },
+    { x: 120, y: 100, z: 260 },
+    { x: 170, y: 300, z: 400 },
+    { x: 140, y: 250, z: 280 },
+    { x: 150, y: 400, z: 500 },
+    { x: 110, y: 280, z: 200 },
+  ];
+
   return (
     <Stack 
       spacing={4} 
@@ -50,9 +64,30 @@ export default function healthPage() {
       }}>
         <h1 style={{ fontWeight: "bold", fontSize:"24px" }}>Health page under construction. Coming 2025 ٩(^ᗜ^ )و </h1>
 
-        
+
+      
       </div>
+
+      <ResponsiveContainer width="100%" height={400}>
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter name="A school" data={data} fill="#8884d8" />
+        </ScatterChart>
+      </ResponsiveContainer>
+        
       {/*****************************************End of main body section*****************************************/}
     </Stack>
+
+    
   );
 }
