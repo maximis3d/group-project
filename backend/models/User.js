e067a4773e7d84aaef03f8fed7a4961b2d27e0fc
maximis3d <marstonvisuals@gmail.com>
2024-11-13 13:03:20 +0000

    Add back other schema fields

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     password: {type: String, required: true},
+    age: { type: Number, required: true },
+    dob: {type: Date, required: true},
+    weight: {type: Number, required: true},
+    activity: {type: String, required: true, enum: ["Not Active", "Lightly Active", "Moderately Active", "Very Active"]}
 });
 
 userSchema.pre('save', async function (next) {
