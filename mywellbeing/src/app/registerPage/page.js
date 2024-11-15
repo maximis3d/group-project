b3b20ef7955dd3c5630bee65895d5a71f2da7c2f
WillnWill <wwillchans@gmail.com>
2024-11-15 16:58:41 +0000

    Changed the register page to accomodate backend code. Removed MoreInfo page as it is no longer needed.

--- mywellbeing/src/app/registerPage/page.js
+++ mywellbeing/src/app/registerPage/page.js
@@ -5,6 +5,28 @@ import { Input } from "@chakra-ui/react";
 import { Flex } from "@chakra-ui/react";
 import { Checkbox } from "@/components/ui/checkbox";
 
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select";
+
+const activityLevel = [
+  { label: "Not Active (no exercise)", value: "Not Active" },
+  { label: "Slightly active (exercise a couple times a week)", value: "Lightly Active" },
+  { label: "Active (exercise 5x a week for at least 45 mins)", value: "Moderately Active" },
+  { label: "Very active (Vigorous exercise multiple times a week)", value: "Very Active" },
+];
+
+const genderOptions = [
+  { label: "Male", value: "Male" },
+  { label: "Female", value: "Female" },
+  { label: "Other", value: "Other" },
+];
+
 export default function registerPage() {
   return (
     <Stack 
@@ -37,32 +59,75 @@ export default function registerPage() {
 
         <h1 style={{ fontWeight: "bold", fontSize: "24px", width: "100%" }}>Sign up now</h1> 
 
-        <Flex className="inputContainer" gap="4" mt="20px"> 
-          <Field label="First name" color="grey">
-            <Input placeholder="First name" size="md" width="140px" />
-          </Field>
 
-          <Field label="Last name" color="grey">
-            <Input placeholder="Last name" size="md" width="140px" />
-          </Field>
-        </Flex>
 
-        <Field label="Phone number" color="grey" mt="20px"> 
-          <Input placeholder="Enter your phone number" size="md" />
+          <Field label="Username" color="grey" mt="20px"> 
+          <Input placeholder="Enter a username" size="md" />
         </Field>
-
-        <Field label="Date of birth" color="grey" mt="20px"> 
-          <Input placeholder="Enter your date of birth" size="md" />
-        </Field>
-
 
         <Field label="Email" color="grey" mt="20px"> 
           <Input placeholder="Enter your email" size="md" />
         </Field>
 
+
         <Field label="Password" helperText="Use 8 characters or more with a mix of letters, numbers & symbols" color="grey" mt="20px"> 
           <Input placeholder="Enter your password" size="md" type="password" />
         </Field>
+
+
+        <h1 style={{ fontWeight: "bold", fontSize: "24px", width: "100%", marginTop:"20px" }}>More about you</h1> 
+
+        <Field label="Age" color="grey" mt="20px"> 
+          <Input placeholder="Enter your date age" size="md" />
+        </Field>
+
+        <Field label="Date of birth" color="grey" mt="20px"> 
+          <Input placeholder="Enter your date of birth in YYYY-MM-DD" size="md" />
+        </Field>
+
+        <Field label="Weight" color="grey" mt="20px"> 
+          <Input placeholder="Enter your weight in KG" size="md" />
+        </Field>
+
+
+        <Field label="Height" color="grey" mt="20px"> 
+          <Input placeholder="Enter your height in KG" size="md" />
+        </Field>
+
+        <SelectRoot size="md"> {/* You can choose the desired size */}
+          <SelectLabel color="grey" mt="20px">Gender</SelectLabel>
+          <SelectTrigger>
+            <SelectValueText placeholder="Select your gender" color="grey"/>
+          </SelectTrigger>
+          <SelectContent>
+            {genderOptions.map((genderOptions) => (
+              <SelectItem item ={genderOptions} key={genderOptions.value} >
+                {genderOptions.label }
+              </SelectItem >
+            ))}
+          </SelectContent>
+        </SelectRoot>
+
+        <Field label="Calories" color="grey" mt="20px"> 
+          <Input placeholder="Whats current calorie intake" size="md" />
+        </Field>
+        
+
+        <SelectRoot size="md"> {/* You can choose the desired size */}
+          <SelectLabel color="grey" mt="20px">Activity Level</SelectLabel>
+          <SelectTrigger>
+            <SelectValueText placeholder="Select activity level" color="grey"/>
+          </SelectTrigger>
+          <SelectContent>
+            {activityLevel.map((activityLevel) => (
+              <SelectItem item={activityLevel} key={activityLevel.value}>
+                {activityLevel.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectRoot>
+
+        
 
         <Checkbox mt="20px" style={{ alignItems: 'flex-start' }}>
           <span style={{ fontSize: '12px', color: 'grey' }}>
@@ -100,7 +165,7 @@ export default function registerPage() {
         marginTop: "40px"
       }}>
 
-        <Button as={Link} href="/moreInfoPage" size="lg" variant="solid" colorPalette="teal" width="200px">
+        <Button as={Link} href="/loginLoadingPage" size="lg" variant="solid" colorPalette="teal" width="200px">
             Register
         </Button>
 
