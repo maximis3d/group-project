915b774f49bcc45c443c7351c6638c71a5b314dd
Max <marstonvisuals@gmail.com>
2024-11-27 17:44:30 +0000

    Move auth/username into auth route

--- backend/server.js
+++ backend/server.js
@@ -18,29 +18,12 @@ connectDB()
   .then(() => console.log("MongoDB Atlas connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
-// Define a new route to get usernames
-app.get('/api/usernames', async (req, res) => {
-  try {
-    const database = mongoose.connection.db;
-    const collection = database.collection('collectionName'); // Replace with your actual collection name
-
-    const usernames = await collection.find({}, { projection: { username: 1, _id: 0 } }).toArray();
-    res.json(usernames);
-  } catch (error) {
-    console.error("Error fetching usernames:", error);
-    res.status(500).send("Error fetching usernames");
-  }
-});
 
 // Define routes
 app.use('/api/auth', authRoutes); // Only one path for authRoutes
 app.use("/user", userRoutes); // Keep this if /user route is needed
 app.use('/api', apiRoutes); // Integrate ApiApp routes
 
-// Health Check Route (Optional, but recommended)
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK', message: 'Server is running smoothly.' });
-});
 
 // Start the server
 app.listen(PORT, () => {
