304ececd666a8fddf9868c6acff39b821cd85d6f
RonnelSalgado1 <80131320+RonnelSalgado1@users.noreply.github.com>
2024-12-07 13:03:19 +0000

    Created dynamic mood slider in health page

---
+++ mywellbeing/src/app/healthPage/healthslider.js
@@ -0,0 +1,101 @@
+"use client"
+
+import { Stack, Box } from "@chakra-ui/react"
+import { Button } from "@/components/ui/button"
+import { Field } from "@/components/ui/field"
+import { Slider } from "@/components/ui/slider"
+import { Controller, useForm } from "react-hook-form"
+import { CiFaceSmile, CiFaceMeh, CiFaceFrown } from "react-icons/ci"
+
+const MoodSlider = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { value: [40] },
+  })
+
+  const onSubmit = handleSubmit((data) => console.log(data))
+
+  // Function to determine which face to show
+  const getFaceIcon = (value) => {
+    if (value >= 70) {
+      return <CiFaceSmile size={50} color="green" />
+    } else if (value >= 40) {
+      return <CiFaceMeh size={50} color="orange" />
+    } else {
+      return <CiFaceFrown size={50} color="red" />
+    }
+  }
+
+  return (
+    <Box width="100%" maxWidth="400px">
+      <form onSubmit={onSubmit}>
+        <Stack align="center" gap="6" w="100%" p="6">
+          <Controller
+            name="value"
+            control={control}
+            render={({ field }) => (
+              <Field
+                label={
+                  <Stack 
+                    align="center" 
+                    spacing={2} 
+                    width="100%" 
+                    display="flex"
+                    justifyContent="center"
+                  >
+                    <Box 
+                      display="flex" 
+                      justifyContent="center" 
+                      alignItems="center"
+                      width="100%"
+                      marginX="auto"
+                    >
+                      {getFaceIcon(field.value[0])}
+                    </Box>
+                    <Box textAlign="center" width="100%">Slider: {field.value[0]}</Box>
+                  </Stack>
+                }
+                invalid={!!errors.value?.length}
+                errorText={errors.value?.[0]?.message}
+                width="100%"
+                style={{ 
+                  minWidth: '300px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center'
+                }}
+              >
+                <Slider
+                  width="full"
+                  onFocusChange={({ focusedIndex }) => {
+                    if (focusedIndex !== -1) return
+                    field.onBlur()
+                  }}
+                  name={field.name}
+                  value={field.value}
+                  onValueChange={({ value }) => {
+                    field.onChange(value)
+                  }}
+                  style={{ 
+                    width: '100%',
+                    minWidth: '300px',
+                    height: '20px'
+                  }}
+                />
+              </Field>
+            )}
+          />
+
+          <Button size="sm" type="submit">
+            Submit
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  )
+}
+
+export default MoodSlider
--- mywellbeing/src/app/healthPage/page.js
+++ mywellbeing/src/app/healthPage/page.js
@@ -1,6 +1,8 @@
 import { Link, Button, HStack, Stack, Image } from "@chakra-ui/react";
 import { Slider } from "@/components/ui/slider";
 import LineChartComponent from "./linechart";  // Import the chart component
+import ChartGoal from "./goalChart";
+import MoodSlider from "./healthslider";
 
 export default function HealthPage() {
   return (
@@ -25,30 +27,29 @@ export default function HealthPage() {
       </HStack>
       {/*****************************************Header Section End*****************************************/}
       
-      {/* Body Section */}
+      
+        <ChartGoal />
+        
       <div
         className="bodyContainer"
         style={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "flex-start",
-          width: "300px",
-          height: "100px",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          maxWidth: "800px",
+          minHeight: "200px",
           textAlign: "center",
-          marginTop: "40px"
+          marginTop: "100px",
+          padding: "0 50px"
         }}
       >
-        <Image
-          height="200px"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/330px-Smiley.svg.png"
-        />
-        <Slider width="100px" defaultValue={[40]} />
-        <Button>Submit</Button>
+        <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+          <MoodSlider />
+        </div>
       </div>
 
-      {/* Render the LineChartComponent here */}
-      <LineChartComponent />
 
       {/*****************************************Main Body Section End*****************************************/}
     </Stack>
